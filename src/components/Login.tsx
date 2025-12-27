dfb4c8f82774f699a564eae0d285a6b4dc2863cb
Blacklion <ankush8singh@gmail.com>
2025-12-27 13:33:10 +0530

    Fix missing PixelSnow import and improve canvas null checks

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,6 +3,7 @@ import { Mail, Lock, AlertCircle, Loader2, Eye, EyeOff } from 'lucide-react';
 import { loginWithEmail, isFirebaseAvailable } from '../utils/firebase';
 import Logo from './Logo';
 import LanguageSelector from './LanguageSelector';
+import PixelSnow from './PixelSnow';
 import { t } from '../utils/i18n';
 
 interface LoginProps {
